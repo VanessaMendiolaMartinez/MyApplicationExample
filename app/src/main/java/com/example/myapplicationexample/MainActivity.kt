5ee9564935a6a69463e95190178a821389895b45
VanessaMendiolaMartinez <vanemtz0110@gmail.com>
2024-11-28 17:40:12 -0600

    :speech_balloon: Text change

--- app/src/main/java/com/example/myapplicationexample/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationexample/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyApplicationExampleTheme {
-        Greeting("Android")
+        Greeting("Android Example")
     }
 }
